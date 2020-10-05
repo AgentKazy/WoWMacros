339897145b01aadcde0a967b7e729c24a036f391
AgentKazy <kazygaming@outlook.pt>
2020-10-05 15:43:17 +0100

    Update Hunter.lua

--- Hunter.lua
+++ Hunter.lua
@@ -1,10 +1,11 @@
 > Damage 1 (Cobra Shot, Steady Shot, Serpent Sting)
 #showtooltip
-/startattack
+/targetenemy [noexists][help][dead]
 /cast [spec:1,@mouseover,harm,nodead][spec:1]Cobra Shot; [spec:2,@mouseover,harm,nodead][spec:2]Steady Shot; [spec:3,@mouseover,harm,nodead][spec:3]Serpent Sting
 
 > Damage 2 (Aimed Shot, Kill Command, Call Pet)
 #showtooltip
+/targetenemy [noexists][help][dead]
 /cast [spec:2]Aimed Shot; [nospec:2,pet]Kill Command; [spec:1]Call Pet 1; [spec:3]Call Pet 2
 /petattack [pet]
 /cast [pet]Dash
@@ -14,6 +15,7 @@
 
 > Damage 3 (Barbed Shot, Rapid Fire, Wildfire Bomb)
 #showtooltip
+/targetenemy [noexists][help][dead] 
 /cast [spec:1]Barbed Shot; [spec:2]Rapid Fire; [spec:3,@mouseover,harm,nodead][spec:3]Wildfire Bomb;
 /petattack [pet]
 /cast [pet]Claw
